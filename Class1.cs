c045c75e48ff0b0d02ad8d63c35817e1083758c8
nistorioan <44305726+nistorioan@users.noreply.github.com>
2019-07-07 13:10:06 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿# include <iostream>
+# include <vector>
+
+using namespace std;
+
+class Animal
+{
+	private:
+        int height;
+	int weight;
+	string name;
+
+	public:
+        int getHeight() { return height; }
+	int getWeight() { return weight; }
+	string getName() { return name; }
+	vector<Animal>* friends;
+	void print_friends()
+	{
+		for (Animal a : *friends)
+		{
+			cout << a.getName() << endl;
+		}
+	};
+	Animal(int height, int weight, string name) :
+
+		height(height), weight(weight), name(name) { };
+
+};
+
+class Dog : public Animal {
+
+    private:
+        string sound = "woof";
+public:
+        void getSound() { cout << sound << endl; }
+Dog(int h, int w, string n) : Animal(h, w, n) { };
+};
+
+void set_friends(Dog& d)
+{
+	Animal b(1, 2, "bb");
+	vector<Animal> d_friends;
+	d_friends.push_back(b);
+	d.friends = &d_friends;
+}
+int main()
+{
+	Dog d(1, 2, "doggy");
+	set_friends(d);
+	d.print_friends();
+	return 0;
+}
+c++ beginner object-oriented
